8457fa50fb594ed6efd387bc266c10fd77a1f63f
Milivoje <milivoje@bitlab.rs>
2019-06-18 16:22:21 +0200

    Update

---
+++ src/models/BaseModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miliv
+ * Date: 6/18/2019
+ * Time: 4:21 PM
+ */
+
+namespace ItVision\ModManager\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+
+}
---
+++ src/models/Manager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miliv
+ * Date: 6/18/2019
+ * Time: 4:20 PM
+ */
+
+namespace ItVision\ModManager\Models;
+
+class Manager extends BaseModel
+{
+
+}

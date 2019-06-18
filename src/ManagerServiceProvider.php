7994b6b0d325a9a8b065c3722f22c4639488ac0c
Milivoje <milivoje@bitlab.rs>
2019-06-18 16:09:52 +0200

    First commit

---
+++ src/ManagerServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miliv
+ * Date: 6/18/2019
+ * Time: 4:04 PM
+ */
+
+namespace ItVision\ModManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class ManagerServiceProvider extends ServiceProvider
+{
+
+
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/database');
+        $this->loadViewsFrom(__DIR__ . '/views', 'backup');
+    }
+
+    public function register()
+    {
+
+    }
+
+
+}

f00960094e1eb56f47c539c7eb145831b693edc7
Milivoje <ivicmilivoje422@gmail.com>
2019-06-30 11:46:28 +0200

    Update

--- src/http/ModCategoryController.php
+++ src/http/ModCategoryController.php
@@ -31,10 +31,6 @@ class ModCategoryController extends Controller
      */
     public function create()
     {
-        echo "<pre>";
-        print_r('asdfasdf');
-        die;
-
         return view('modManager::categoryCreate');
     }
 

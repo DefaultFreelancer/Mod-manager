02f3d95920a8f18edac606dda4ea2ad2f642200b
Milivoje <ivicmilivoje422@gmail.com>
2019-07-05 07:53:37 +0200

    Update

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,17 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: miliv
- * Date: 6/18/2019
- * Time: 4:33 PM
- */
 
 Route::group(['prefix' => 'server/{server}/mods','namespace' => 'ItVision\ModManager\http', 'middleware' => ['web', 'csrf', 'auth', 'server', 'subuser.auth', 'node.maintenance']], function () {
     Route::get('/', 'ModController@index')->name('server.modmanager.index');
     Route::post('/install/{mod}', 'ModController@install')->name('server.modmanager.install');
     Route::post('/remove/{mod}', 'ModController@remove')->name('server.modmanager.remove');
 });
-
 
 Route::group(['prefix' => '/admin', 'namespace'=>'ItVision\ModManager\http\admin', 'middleware' => ['web','auth']], function (){
     Route::resource('mod', 'AdminModController');
